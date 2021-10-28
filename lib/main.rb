a10788963e38024c28136f5804a4aa8265606d2a
berryStraw <marius1357975@gmail.com>
2021-10-28 16:02:14 +0100

    finished git project

--- lib/main.rb
+++ lib/main.rb
@@ -1,9 +1,43 @@
 require_relative 'tree'
 
-array=[1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-array=[1,2,3,4,5,6,7,8,9]
-array=[1,2,4,5,8,10]
-tree=Tree.new(array)
+#array=[1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+#array=[1,2,3,4,5,6,7,8,9]
+#array=[1,2,4,5,8,10]
+#tree=Tree.new(array)
 #tree.insert(3)
 #tree.delete(1)
-p tree.find(10)
+#p tree.find(10)
+#tree.level_order{|val| p val}
+#p tree.level_order()
+
+#tree.preorder{|num| puts num}
+#tree.inorder{|num| puts num}
+#tree.postorder{|num| puts num}
+
+#p tree.preorder()
+#p tree.inorder()
+#p tree.postorder()
+
+#tree.height(8)
+#p tree.depth(6345)
+#p tree.balanced?()
+#tree.rebalance()
+
+newArray=(Array.new(15) { rand(1..100) })
+newTree=Tree.new(newArray)
+p newTree.balanced?()
+p newTree.level_order()
+p newTree.preorder()
+p newTree.postorder()
+p newTree.inorder()
+newTree.insert(100)
+newTree.insert(102)
+newTree.insert(101)
+newTree.insert(103)
+p newTree.balanced?()
+newTree.rebalance()
+p newTree.balanced?()
+p newTree.level_order()
+p newTree.preorder()
+p newTree.postorder()
+p newTree.inorder()
--- lib/tree.rb
+++ lib/tree.rb
@@ -21,14 +21,20 @@ class Tree
         node=@root
         while node.left_node!=nil || node.right_node!=nil
             if val<node.value
+                if node.left_node==nil
+                    break
+                end
                 node=node.left_node
-            else
+            elsif val>node.value
+                if node.right_node==nil
+                    break
+                end
                 node=node.right_node
             end
         end
         if val<node.value
             node.left_node=Node.new(val)
-        else
+        elsif val>node.value
             node.right_node=Node.new(val)
         end
     end
@@ -81,5 +87,110 @@ class Tree
             end
         end
         return node
+    end
+
+    def level_order(&block)
+        que=[@root]
+        arr=[]
+        while !que.empty?
+            current=que[0]
+            que.push(current.left_node,current.right_node)
+            if block_given?
+                block.call(current.value)
+            else
+                arr<<current.value
+            end
+            que.compact!
+            que.shift
+        end
+        arr
+    end
+    def inorder(arr=[],tree=@root,&block)
+        return if tree==nil
+        if block_given?
+            inorder(arr,tree.left_node,&block)
+            block.call(tree.value)
+            inorder(arr,tree.right_node,&block)
+        else
+            arr<<inorder(arr,tree.left_node,&block)
+            arr<<tree.value
+            inorder(arr,tree.right_node,&block)
+            arr=arr.select{|item| item unless item.kind_of?(Array)}
+        end
+        
+    end
+
+    def preorder(arr=[],tree=@root,&block) 
+        return if tree==nil
+        if block_given?
+            block.call(tree.value)
+            preorder(arr,tree.left_node,&block)
+            preorder(arr,tree.right_node,&block)
+        else
+            arr<<tree.value
+            arr<<preorder(arr,tree.left_node,&block)
+            arr<<preorder(arr,tree.right_node,&block)
+            arr=arr.select{|item| item unless item.kind_of?(Array)}
+        end
+        
+    end
+
+    def postorder(arr=[],tree=@root,&block)
+        return if tree==nil
+        postorder(arr,tree.left_node,&block)
+        postorder(arr,tree.right_node,&block)
+        if block_given?
+            block.call(tree.value)
+        else
+            arr<<tree.value
+        end
+    end
+
+    def height(node,h=0, new=0)
+        node=find(node) if node.kind_of?(Numeric)
+        if node.left_node==nil && node.right_node==nil
+            h
+        elsif node.right_node!=nil
+            h+=1
+            if new>h
+                h=new
+            end
+            new=height(node.right_node,h)
+        elsif node.left_node!=nil
+            h+=1
+            if new>h
+                h=new
+            end
+            new=height(node.left_node,h)
+        end
+    end
+
+    def depth(val,d=0)
+        node=@root
+        while node.value!=val
+            if val<node.value
+                d+=1
+                node=node.left_node
+            else
+                d+=1
+                node=node.right_node
+            end
+        end
+        return d
+    end
+
+    def balanced?()
+        lh=height(@root.left_node)
+        rh=height(@root.right_node)
+        diff=lh-rh if lh==rh
+        diff=lh-rh if lh>rh
+        diff=rh-lh if lh<rh
+        return diff<2
+    end
+
+    def rebalance()
+        newArray=level_order()
+        newArray.sort!().uniq!
+        @root=build_tree(newArray,0,newArray.length-1)
     end
 end
